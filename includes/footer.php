c804b653c681cc686d9237cd74c2a78f671c958e
Nesitha Perera <nesithaperera4916@gmail.com>
2026-09-10 09:59:09 +0530

    footer

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<footer class="site-footer">
+
+    <div class="footer-container">
+
+        <p>
+            &copy; <?php echo date("Y"); ?> NSBM Marketplace.
+            All rights reserved.
+        </p>
+
+        <p>
+            NSBM Marketplace - Buy and Sell Products
+        </p>
+
+    </div>
+
+</footer>
